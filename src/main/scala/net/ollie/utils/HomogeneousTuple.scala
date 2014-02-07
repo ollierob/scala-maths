3182a7c61a8f4856dff77c18c78722c2f6d61486
Ollie Robertshaw <ollie.robertshaw@gmail.com>
2014-02-07 12:48:12 +0000

    Homogeneous 2-tuple

--- src/main/scala/net/ollie/utils/HomogeneousTuple.scala
+++ src/main/scala/net/ollie/utils/HomogeneousTuple.scala
@@ -4,7 +4,7 @@ package net.ollie.utils
  * Created by Ollie on 06/02/14.
  */
 trait HomogeneousTuple[+T]
-extends Product {
+        extends Product {
 
     override def productIterator: Iterator[T] = iterator
 
@@ -24,7 +24,7 @@ class Homogeneous1Tuple[T](a: T)
         extends Tuple1(a)
         with HomogeneousTuple[T] {
 
-    def size = 1
+    final def size = 1
 
     def iterator = new Iterator[T] {
 
@@ -42,6 +42,37 @@ class Homogeneous1Tuple[T](a: T)
 
 }
 
+object Homogeneous2Tuple {
+
+    implicit def apply[T](tuple: (T, T)) = new Homogeneous2Tuple(tuple._1, tuple._2)
+
+}
+
+class Homogeneous2Tuple[+T](a: T, b: T)
+        extends Tuple2(a, b)
+        with HomogeneousTuple[T] {
+
+    final def size = 2
+
+    def iterator = new Iterator[T] {
+
+        var index = 0;
+
+        override def hasNext: Boolean = index < 3
+
+        override def next(): T = {
+            index += 1
+            index match {
+                case 1 => a
+                case 2 => b
+                case _ => throw new NoSuchElementException
+            }
+        }
+
+    }
+
+}
+
 object Homogeneous3Tuple {
 
     implicit def apply[T](tuple: (T, T, T)) = new Homogeneous3Tuple(tuple._1, tuple._2, tuple._3)
@@ -52,7 +83,9 @@ class Homogeneous3Tuple[T](a: T, b: T, c: T)
         extends Tuple3(a, b, c)
         with HomogeneousTuple[T] {
 
-    override def iterator = new Iterator[T] {
+    final def size = 3
+
+    def iterator = new Iterator[T] {
 
         var index = 0;
 
@@ -69,7 +102,5 @@ class Homogeneous3Tuple[T](a: T, b: T, c: T)
         }
 
     }
-
-    def size = 3
 
 }
