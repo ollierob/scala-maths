422ec21a4e517e8d00fa27abc2d5698bad077c72
Ollie Robertshaw <ollie.robertshaw@gmail.com>
2014-02-19 20:31:03 +0000

    Riemann function for integers

--- src/main/scala/net/ollie/maths/functions/polylogarithmic/RiemannZeta.scala
+++ src/main/scala/net/ollie/maths/functions/polylogarithmic/RiemannZeta.scala
@@ -1,70 +1,75 @@
-//package net.ollie.maths.functions.polylogarithmic
-//
-//import net.ollie.maths.Expression
-//import net.ollie.maths.functions.{RealFunctionBuilder, BuiltFunction}
-//import net.ollie.maths.numbers.{Natural, Precision, Real, Integer}
-//import net.ollie.maths.methods.Series
-//import net.ollie.maths.numbers.constants.{Zero, Half, One}
-//import net.ollie.maths.functions.numeric.Ln
-//
-///**
-// * Created by Ollie on 17/02/14.
-// * @see http://mathworld.wolfram.com/RiemannZetaFunction.html
-// */
-//object RiemannZeta
-//        extends RealFunctionBuilder {
-//
-//    def apply(re: Real): Real = re match {
-//        case Zero => empty
-//        case i: Integer => apply(i)
-//        case _ => ???
-//    }
-//
-//    def apply(i: Integer): Real = {
-//        if (i.isEmpty) empty
-//        else new IntegerZeta(i)
-//    }
-//
-//    protected[this] def empty = -Half
-//
-//    protected[this] def create(expr: Expression) = new RiemannZetaOf(expr)
-//
-//}
-//
-//trait RiemannZeta {
-//
-//    def of: Expression
-//
-//    override def toString = s"RiemannZeta($of)"
-//
-//}
-//
-//class RiemannZetaOf(val of: Expression)
-//        extends BuiltFunction
-//        with RiemannZeta {
-//
-//    def isEmpty = false
-//
-//    protected[this] def derivative(x: Expression) = -Series(dk _, 2)
-//
-//    private def dk(k: Integer): Expression = Ln(k) / (k ^ of)
-//
-//    override protected[this] def builder = RiemannZeta
-//
-//}
-//
-//class IntegerZeta(val of: Integer)
-//        extends Real
-//        with RiemannZeta {
-//
-//    require(!of.isEmpty)
-//
-//    def isEmpty = false
-//
-//    private lazy val series = Series(nth _, One)
-//
-//    private def nth(k: Natural): Real = 1 / (k ^ of)
-//
-//    protected[this] def doEvaluate(precision: Precision) = series.evaluate(precision)
-//
-//}
+package net.ollie.maths.functions.polylogarithmic
+
+import net.ollie.maths.Expression
+import net.ollie.maths.functions.{RealFunctionBuilder, BuiltFunction}
+import net.ollie.maths.numbers._
+import net.ollie.maths.methods.Series
+import net.ollie.maths.numbers.constants._
+import net.ollie.maths.functions.numeric.Ln
+import net.ollie.maths.sequences.BernoulliSequence
+
+/**
+ * Created by Ollie on 17/02/14.
+ * @see http://mathworld.wolfram.com/RiemannZetaFunction.html
+ */
+object RiemannZeta
+        extends RealFunctionBuilder {
+
+    def apply(re: Real): Real = re match {
+        case Zero => empty
+        case i: Integer => apply(i)
+        case _ => ???
+    }
+
+    def apply(i: Integer): Real = i match {
+        case Zero => empty
+        case _ if !i.isStrictlyPositive => -BernoulliSequence(i.abs.succ) / i.abs.succ
+        case One => Infinity
+        case _ if i.isEven => new EvenIntegerZeta(i.abs)
+        case _ => Zero
+    }
+
+    protected[this] def empty = -Half
+
+    protected[this] def create(expr: Expression) = new RiemannZetaOf(expr)
+
+}
+
+trait RiemannZeta {
+
+    def of: Expression
+
+    override def toString = s"RiemannZeta($of)"
+
+}
+
+class RiemannZetaOf(val of: Expression)
+        extends BuiltFunction
+        with RiemannZeta {
+
+    def isEmpty = false
+
+    protected[this] def derivative(x: Expression) = -Series(dk _, 2)
+
+    private def dk(k: Integer): Expression = Ln(k) / (k ^ of)
+
+    override protected[this] def builder = RiemannZeta
+
+}
+
+private class EvenIntegerZeta(val of: Natural)
+        extends Real
+        with RiemannZeta {
+
+    require(!of.isEmpty)
+    require(of.isEven)
+
+    def isEmpty = false
+
+    private lazy val halfN: Natural = (of / 2).numerator.abs
+
+    private lazy val rep: Real = (MinusOne ^ (halfN.succ)) * BernoulliSequence(of) * ((2 * Pi) ^ of) / (2 * (of !))
+
+    protected[this] def doEvaluate(precision: Precision) = rep.evaluate(precision)
+
+}
